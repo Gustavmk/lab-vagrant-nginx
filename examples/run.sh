b3c0e9d87131288d1888780cd3d466416a111b65
Kuno <gustavo.kuno@linx.com.br>
2023-04-12 18:18:14 -0300

    initial commit

---
+++ examples/run.sh
@@ -0,0 +1,15 @@
+# BASE 
+ansible-playbook -i ansible/inv.yml ansible/pb-docker.yml 
+
+# DB
+ansible-playbook -i ansible/inv.yml ansible/pb-db.yml 
+
+# WEB 
+ansible-playbook -i ansible/inv.yml ansible/pb-web.yml
+
+
+# LB
+ansible-galaxy install nginxinc.nginx
+ansible-playbook -i ansible/inv.yml ansible/pb-lb-role.yml
+ansible-playbook -i ansible/inv.yml ansible/pb-lb-config.yml
+ansible-playbook -i ansible/inv.yml ansible/pb-lb-monitoring.yml
